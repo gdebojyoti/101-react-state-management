32641c0e5278a0dc7ddd44cbc486ededa2c069c5
Debojyoti Ghosh <gdebojyoti@users.noreply.github.com>
2025-02-09 18:05:59 +0530

    Home page updated to consume redux store.

--- redux-toolkit/src/pages/Home/index.jsx
+++ redux-toolkit/src/pages/Home/index.jsx
@@ -1,6 +1,18 @@
+import { useSelector, useDispatch } from 'react-redux'
+
 import Nav from "../../components/Nav"
+import { updateAllProducts } from '../../store/slices/homeSlice'
+import { useEffect } from 'react'
 
 const HomePage = () => {
+  const allProducts = useSelector((state) => state.home.allProducts)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(updateAllProducts([1, 2, 3]))
+  }, [])
+
+  console.log(allProducts)
   return (
     <div>
       <Nav />
