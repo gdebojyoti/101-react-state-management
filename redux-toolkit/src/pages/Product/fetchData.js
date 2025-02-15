c5d3fba35b8a468de3da07452c99aa5b7ab9ae8e
Debojyoti Ghosh <gdebojyoti@users.noreply.github.com>
2025-02-16 03:48:22 +0530

    Product page updated.

--- redux-toolkit/src/pages/Product/fetchData.js
+++ redux-toolkit/src/pages/Product/fetchData.js
@@ -3,9 +3,11 @@ import products from 'src/data/products.json'
 import { updateDetails } from 'src/store/slices/productSlice'
 
 export default function fetchData (dispatch, id) {
-  const item = products.find(({ key }) => key === id)
-  dispatch(updateDetails({
-    isLoaded: true,
-    data: item
-  }))
+  setTimeout(() => {
+    const item = products.find(({ key }) => key === id)
+    dispatch(updateDetails({
+      isLoaded: true,
+      data: item
+    }))
+  }, 1000)
 }
--- redux-toolkit/src/pages/Product/index.jsx
+++ redux-toolkit/src/pages/Product/index.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from 'react'
 import { useParams } from 'react-router'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 
 import Header from "src/components/Header"
 
@@ -10,17 +10,32 @@ const Product = () => {
   const { id } = useParams()
   const dispatch = useDispatch()
 
+  // do this only if 100% sure that all fields (isLoaded, data) will be updated together - thereby not causing unnecessary re-renders
+  const { isLoaded, data } = useSelector((state) => state.product)
+
   useEffect(() => {
     fetchData(dispatch, id)
   }, [])
 
+  console.log(isLoaded, data)
+
+  const { title, thumbnailUrl, price } = data || {}
+
   return (
     <div>
       <Header />
-      <h1>Product page</h1>
-      <div>
-        Book ID: {id}
-      </div>
+
+      {!isLoaded && <div>Loading..</div>}
+
+      {isLoaded && (
+        !data
+          ? (<div>No product found for ID {id}</div>)
+          : (<div>
+              <h1>{title}</h1>
+              <img src={thumbnailUrl} />
+              <div>Price: USD {price}</div>
+            </div>)
+      )}
     </div>
   )
 }
